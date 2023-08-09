4966a480c1f3ccd5bd30590ead6909238edd2cdc
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-09 17:19:55 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,21 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
+    `maven-publish`
 }
 
 group = "studio.hcmc"
-version = "1.0-SNAPSHOT"
+version = "0.0.4-alpha01"
 
 repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.5.1")
-}
-
-kotlin {
-    jvmToolchain(17)
 }
