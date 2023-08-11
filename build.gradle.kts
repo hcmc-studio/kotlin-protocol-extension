823c5001db178123b07947f676c5a3b836ab1b7f
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-12 00:41:08 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "studio.hcmc"
-version = "0.0.8"
+version = "0.0.9"
 
 repositories {
     mavenCentral()
@@ -20,7 +20,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "studio.hcmc"
             artifactId = "kotlin-protocol-extension"
-            version = "0.0.8"
+            version = "0.0.9"
             from(components["java"])
         }
     }
