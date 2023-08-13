3eda8cb5fbcdbeaa93b251576843ae0a80d434e7
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-13 19:53:49 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+val kotlin_version: String by project
 val project_version: String by project
 val jdk_version: String by project
 val kotlinx_datetime_version: String by project
@@ -33,6 +34,7 @@ publishing {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:$kotlinx_datetime_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$kotlinx_serialization_version")
 }
--- src/main/kotlin/studio/hcmc/kotlin/protocol/DataTransferObject.kt
+++ src/main/kotlin/studio/hcmc/kotlin/protocol/DataTransferObject.kt
@@ -1,3 +1,25 @@
 package studio.hcmc.kotlin.protocol
 
 interface DataTransferObject
+
+/**
+ * [DTO]에 정의된 값을 대상 객체에 적용
+ */
+inline fun <reified DTO> DTO.applyNotNullTo(o: Any): Int where DTO : DataTransferObject {
+    var applied = 0
+    val thisGetters = this::class.java.declaredMethods.filter { it.name.startsWith("get") }
+    val destSetters = o::class.java.declaredMethods.filter { it.name.startsWith("set") }
+    for (thisGetter in thisGetters) {
+        if (!thisGetter.name.startsWith("get")) {
+            continue
+        }
+
+        val setterName = "set${thisGetter.name.removePrefix("get")}"
+        val v = thisGetter.invoke(this) ?: continue
+        val setter = destSetters.find { it.name == setterName } ?: continue
+        setter.invoke(o, v)
+        applied++
+    }
+
+    return applied
+}
