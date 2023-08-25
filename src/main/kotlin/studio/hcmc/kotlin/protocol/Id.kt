c9aedae0886b3da7b78a1b54d37235913e93898a
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-25 17:53:32 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/kotlin/protocol/Id.kt
+++ src/main/kotlin/studio/hcmc/kotlin/protocol/Id.kt
@@ -1,11 +1,5 @@
 package studio.hcmc.kotlin.protocol
 
-import kotlin.reflect.full.primaryConstructor
-
 interface Id<V> {
     val value: V
-}
-
-inline fun <V, reified Id> V.id(): Id where Id : studio.hcmc.kotlin.protocol.Id<V> {
-    return Id::class.primaryConstructor!!.call(this)
 }
--- src/main/kotlin/studio/hcmc/kotlin/protocol/io/DataTransferObject.kt
+++ src/main/kotlin/studio/hcmc/kotlin/protocol/io/DataTransferObject.kt
@@ -1,26 +1,37 @@
 package studio.hcmc.kotlin.protocol.io
 
+import studio.hcmc.kotlin.protocol.Id
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+
 interface DataTransferObject
 
 /**
  * [DTO]에 정의된 값을 대상 객체에 적용
  */
-inline fun <reified DTO> DTO.applyNotNullTo(o: Any): Int where DTO : DataTransferObject {
+@Suppress("UNCHECKED_CAST")
+inline fun <reified DTO, reified Destination : Any> DTO.applyNotNullTo(
+    o: Destination,
+    vararg excludes: String
+): Int where DTO : DataTransferObject {
     var applied = 0
-    val thisGetters = this::class.java.declaredMethods.filter { it.name.startsWith("get") }
-    val destSetters = o::class.java.declaredMethods.filter { it.name.startsWith("set") }
+    val thisGetters = this::class.memberProperties
+        .filterIsInstance<KProperty1<DTO, *>>()
+        .filter { property -> excludes.none { it == property.name } }
+    val destSetters = o::class.memberProperties
+        .filterIsInstance<KMutableProperty1<Destination, *>>()
+        .filter { property -> excludes.none { it == property.name } }
     for (thisGetter in thisGetters) {
-        if (!thisGetter.name.startsWith("get")) {
-            continue
+        val v = thisGetter.get(this) ?: continue
+        val destSetter = destSetters.find { it.name == thisGetter.name } ?: continue
+        if (v is Id<*>) {
+            (destSetter as KMutableProperty1<Destination, Any?>).set(o, v.value)
+        } else {
+            (destSetter as KMutableProperty1<Destination, Any?>).set(o, v)
         }
 
-        val setterName = "set${thisGetter.name.removePrefix("get")}"
-        val v = thisGetter.invoke(this) ?: continue
-        val setter = destSetters.find { it.name == setterName } ?: continue
-        try {
-            setter.invoke(o, v)
-            applied++
-        } catch (_: IllegalArgumentException) {}
+        applied++
     }
 
     return applied
