1fd89a3b3ab101df45b322484df6cc4e275beffb
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-25 22:10:17 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/kotlin/protocol/Id.kt
+++ src/main/kotlin/studio/hcmc/kotlin/protocol/Id.kt
@@ -1,5 +1,15 @@
 package studio.hcmc.kotlin.protocol
 
+import kotlin.reflect.full.primaryConstructor
+
+private typealias _Id<V> = Id<V>
+
 interface Id<V> {
     val value: V
+
+    companion object {
+        inline fun <V, reified Id : _Id<V>> wrap(value: V): Id {
+            return Id::class.primaryConstructor!!.call(value)
+        }
+    }
 }
